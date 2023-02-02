177defd8f2c974b18fee0642cceb5e32053b7fe2
ProximaXD <panmieciu762@gmail.com>
2023-02-02 22:54:16 +0100

    Update repository

--- QuestionManager.cs
+++ QuestionManager.cs
@@ -8,9 +8,10 @@ public class QuestionManager : MonoBehaviour
     static string filePath = "C:/test.txt";                     //*********Source*********
 
     [Header("Quiz file settings")]
-    [SerializeField] int countOfAnswers;          //Count of answers after Question
-    public string[] questions;      //variable with questions   [not edit]
-    public string[] answers;        //variable with answers     [not edit]
+    [SerializeField] int countOfAnswers;            //Count of answers after Question
+
+    public string[] questions;                      //variable with questions   [not edit]
+    public string[] answers;                        //variable with answers     [not edit]
 
 
     int spaceBetweenQuestions;    
@@ -24,19 +25,21 @@ public class QuestionManager : MonoBehaviour
 
     void declareQandA()
     {
-        int countOfQuestions = lines.Length / (countOfAnswers + 1);
-        questions = new string[countOfQuestions];                   //declare count of questions
-        answers = new string[countOfQuestions * countOfAnswers];    //declare count of answers
+        int countOfQuestions = lines.Length / (countOfAnswers + 1);                         //
+        questions = new string[countOfQuestions];                                           //declare count of questions
+        answers = new string[countOfQuestions * countOfAnswers];                            //declare count of answers
 
         for (int i = 0, qIndex = 0, aIndex = 0; i < lines.Length; i += countOfAnswers + 1)
         {
             questions[qIndex] = lines[i];                                                   //Set question[] 
             for (int j = i + 1; j < i + countOfAnswers + 1 && j < lines.Length; j++)
             {
-                answers[aIndex] = lines[j];                                             //Set answer[]  
+                answers[aIndex] = lines[j];                                                 //Set answer[]  
                 aIndex++;
             }
             qIndex++;
         }
     }
 }
+
+/*Made by Proxima:Wiktoruskowy2000
---
+++ README.txt
@@ -0,0 +1,5 @@
+This is the most simple code for a question and answer manager that I could write.
+Using this code is very straightforward, but you must keep in mind that it does not remove empty answers. 
+For example, if you declare that there should be 7 answers under a question, you must enter 7 answers. I will try to update this function in my next commit.
+
+The code is unlicensed, so you can use it for commercial purposes.
